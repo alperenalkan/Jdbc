0a3705d235568daebb049faf4d85a0e0f84869a7
Alperen Alkan <alpernkaligrafi@gmail.com>
2025-07-08 21:19:29 +0200

    first commit

---
+++ src/main/java/com/tpe/jdbc/Execute01.java
@@ -0,0 +1,13 @@
+package com.tpe.jdbc;
+
+public class Execute01 {
+    //STEP 1: Driver ekleme - JDK7'den sonra redundant
+
+    //STEP 2: Veritabani adresi, login bilgileri ekleme
+
+    //STEP 3: Statement olusturma - Query execute etmek icin kullanilir.
+
+    //STEP 4: Query olusturma
+
+    //STEP 5: Kaynaklari salmak
+}
